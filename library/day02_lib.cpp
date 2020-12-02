dd6983a3ad1f973a9747b176fb204a9bd59c47a3
Butch Howard <butch.howard@accenture.com>
2020-12-02 09:43:46 -0500

    Day02 test passes

--- library/day02_lib.cpp
+++ library/day02_lib.cpp
@@ -1,8 +1,14 @@
 #include "day02_lib.h"
+#include <algorithm>
 
 int day02_part1_solve(const PasswordData& password_data)
 {
-    (void)password_data;
-    
-    return -1;
+
+    return std::ranges::count_if(password_data.begin(), password_data.end(),
+                                [](const PasswordItem& p)
+                                { 
+                                    auto c = std::ranges::count(p.password.begin(), p.password.end(), p.letter);
+                                    return c >= p.min_count && c <= p.max_count; 
+                                });
+
 }
